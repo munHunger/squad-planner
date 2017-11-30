48837c441fe77d3d18f220b43eccd4610416b9ba
munHunger <marcusmunger@hotmail.com>
2017-11-30 19:27:07 +0100

    base routing added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
+import { AppComponent } from './components/app/app.component';
+import { LoginComponent } from './components/login/login.component';
 
-import { AppComponent } from './app.component';
-
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
